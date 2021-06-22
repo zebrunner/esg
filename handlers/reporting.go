42207e287eeb91b9d5ebaaacab57c9f4791a3b6f
Anatoliy Platonov <p4m.dev@gmail.com>
2021-06-22 14:56:33 +0000

    Rework status response

--- handlers/reporting.go
+++ handlers/reporting.go
@@ -1,32 +1,50 @@
 package handlers
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/zebrunner/esg/service"
 	"net/http"
+	"os"
+	"runtime"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 var (
 	startTime time.Time
+	Revission = "undefined"
+	BuildTime = "undefined"
+	Version   = "undefined"
 )
 
 func init() {
 	startTime = time.Now()
+	Revission = os.Getenv("REVISSION")
+	BuildTime = os.Getenv("BUILD_TIME")
+	Version = os.Getenv("VERSION")
 }
 
 func Ping(c *gin.Context) {
 	c.JSON(http.StatusOK, map[string]interface{}{
-		"uptime": time.Since(startTime),
+		"uptime":  time.Since(startTime),
 		"version": "1.0.0", // TODO: Get current version for project
 	})
 }
 
 func ClusterStatus(c *gin.Context) {
-	status, err := service.GetTasksCount()
-	if err != nil {
-		c.Error(err)
-		return
+	status := gin.H{
+		"status": 0,
+		"value": gin.H{
+			"ready":   true,
+			"message": "Server is running",
+			"build": gin.H{
+				"revission": Revission,
+				"time":      BuildTime,
+				"version":   Version,
+			},
+			"go": gin.H{
+				"version": runtime.Version(),
+			},
+		},
 	}
 	c.JSON(http.StatusOK, status)
 }
