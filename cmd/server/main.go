ee75cbdddfa362b096486484b5eb2a9d9a6a3ed0
Anatoliy Platonov <p4m.dev@gmail.com>
2021-08-12 14:45:00 +0000

    Proxy head to fileserver

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -119,6 +119,7 @@ func CreateRouter() *gin.Engine {
 
 		hub.GET("/download/:session/:file", handlers.Downloads)
 		hub.DELETE("/download/:session/:file", handlers.Downloads)
+		hub.HEAD("/download/:session/:file", handlers.Downloads)
 
 		hub.GET("/clipboard/:session", handlers.Clipboard)
 		hub.POST("/clipboard/:session", handlers.Clipboard)
