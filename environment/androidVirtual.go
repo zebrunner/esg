f86bad8d56f3de30f4215e98fde02008c0e494f1
Anatoliy Platonov <p4m.dev@gmail.com>
2022-03-23 11:06:16 +0000

    switched to 1.3-beta2 appium

--- environment/androidVirtual.go
+++ environment/androidVirtual.go
@@ -35,7 +35,7 @@ func buildAppiumRedroid(workspace string, caps *selenium.Capabilities, conf *con
 	deviceContainer.SetMemory(caps.Memory)
 	deviceContainer.SetMemoryReservation(caps.MemoryReservation)
 
-	appiumImage := imageRepo + "appium:1.2-beta9"
+	appiumImage := imageRepo + "appium:1.3-beta2"
 	appiumContainer := Container{
 		Name:              "appium",
 		Image:             appiumImage,
