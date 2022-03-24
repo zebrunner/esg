d255522cc30581fb2a1d2ebc243402592c43fd10
Anatoliy Platonov <p4m.dev@gmail.com>
2022-03-24 15:49:53 +0000

    bump up to 1.3-beta9 appium

--- environment/androidVirtual.go
+++ environment/androidVirtual.go
@@ -35,7 +35,7 @@ func buildAppiumRedroid(workspace string, caps *selenium.Capabilities, conf *con
 	deviceContainer.SetMemory(caps.Memory)
 	deviceContainer.SetMemoryReservation(caps.MemoryReservation)
 
-	appiumImage := imageRepo + "appium:1.3-beta5"
+	appiumImage := imageRepo + "appium:1.3-beta9"
 	appiumContainer := Container{
 		Name:              "appium",
 		Image:             appiumImage,
