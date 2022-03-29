b8c0fae42b5f5b5f96756e8cb8c7285d087e71f8
Anatoliy Platonov <p4m.dev@gmail.com>
2022-03-29 18:34:49 +0000

    #243: increased service-startup-timeout 4->5m

--- config/config.go
+++ config/config.go
@@ -78,7 +78,7 @@ func init() {
 
 	flag.DurationVar(&Conf.IdleTimeout, "idle-timeout", 60*time.Second, "Session idle timeout in time.Duration format")
 	flag.DurationVar(&Conf.SessionDeleteTimeout, "session-delete-timeout", 30*time.Second, "Session delete timeout in time.Duration format")
-	flag.DurationVar(&Conf.ServiceStartupTimeout, "service-startup-timeout", 4*time.Minute, "Service startup timeout in time.Duration format")
+	flag.DurationVar(&Conf.ServiceStartupTimeout, "service-startup-timeout", 5*time.Minute, "Service startup timeout in time.Duration format")
 	flag.DurationVar(&Conf.InstanceCooldownTimeout, "instance-cooldown-timeout", 1*time.Minute, "Time after instance start when shutdown is prohibited on scale down in time.Duration format")
 
 	flag.StringVar(&Conf.DbConnectionString, "db-connection", "", "Connection string for database")
