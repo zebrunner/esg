998f60bf293d206ced61e1f1305c5f17d03ed682
Anatoliy Platonov <p4m.dev@gmail.com>
2022-04-23 11:07:21 +0000

    #268: bump up to appium 1.4 with improved /status-adb healthcheck

--- environment/androidVirtual.go
+++ environment/androidVirtual.go
@@ -35,7 +35,7 @@ func buildAppiumRedroid(workspace string, caps *selenium.Capabilities, conf *con
 	deviceContainer.SetMemory(caps.Memory)
 	deviceContainer.SetMemoryReservation(caps.MemoryReservation)
 
-	appiumImage := imageRepo + "appium:1.3"
+	appiumImage := imageRepo + "appium:1.4"
 	appiumContainer := Container{
 		Name:              "appium",
 		Image:             appiumImage,
