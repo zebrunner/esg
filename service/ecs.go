f22b1add35aa9e8b0cd8fc3769a352f2b9df0a97
Anatoliy Platonov <p4m.dev@gmail.com>
2022-11-17 21:29:33 +0000

    #366: improved InvalidParameterException error catching process

--- service/ecs.go
+++ service/ecs.go
@@ -376,8 +376,7 @@ out:
 		if err != nil {
 			l.WithError(err).WithField("attempt", i).WithField("latency", time.Since(startTime)).Warn("Failed to run task")
 			outputErr = fmt.Errorf("failed to run task: %v", err)
-			if strings.HasPrefix(err.Error(), "image not found: ")
-				|| strings.HasPrefix(err.Error(), "InvalidParameterException") { //#366 disable retries for InvalidParameterException
+			if strings.HasPrefix(err.Error(), "image not found: ") || strings.HasPrefix(err.Error(), "InvalidParameterException") { //#366 disable retries for InvalidParameterException
 				break out
 			}
 			continue
