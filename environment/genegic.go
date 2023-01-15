a69e443fe648ec5d43761dba925d8446e32fc6d8
Anatoliy Platonov <p4m.dev@gmail.com>
2023-01-15 15:05:59 +0000

    #412: bump up to entrypoint:1.3-beta1

--- environment/genegic.go
+++ environment/genegic.go
@@ -62,7 +62,7 @@ func buildGeneric(workspace string, caps *capabilities.Capabilities, conf *confi
                 EntryPoint: []string{"/bin/sh"},
         }
 
-        entrypointImage := imageRepo + "entrypoint:1.2"
+        entrypointImage := imageRepo + "entrypoint:1.3-beta1"
         entrypointContainer := Container{
                 Name:              "entrypoint",
                 Image:             entrypointImage,
