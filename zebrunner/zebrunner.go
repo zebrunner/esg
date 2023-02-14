8aceeb1f66e21b0ae9f67c5c6f334eb54da5824e
Diego Wald <diego.wald@gmail.com>
2023-02-14 14:43:10 -0300

    issue-452 abort tasks when tasks are stuck

--- zebrunner/zebrunner.go
+++ zebrunner/zebrunner.go
@@ -3,19 +3,22 @@ package zebrunner
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go/service/ecs"
 	"net/http"
 	"net/url"
+	"strconv"
 	"time"
-        "strconv"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/zebrunner/esg/config"
 
-        sessionmap "github.com/zebrunner/esg/sessinonmap"
+	sessionmap "github.com/zebrunner/esg/sessinonmap"
 )
 
 const (
         USAGE_API_PATH = "/api/quota/v2/engine-usages"
+		ABORT_API_PATH = "/api/reporting/api/project-test-runs/abort"
 )
 
 func TrackResourcesUsage(sess *sessionmap.Session, d time.Duration) {
@@ -68,5 +71,73 @@ func TrackResourcesUsage(sess *sessionmap.Session, d time.Duration) {
 		return
 	} else {
 		log.WithField("_taskId", sess.ID).WithField("workspace", sess.Workspace).WithField("request body", requestBody).Info("shape recorded")
+	}
+}
+
+func getAutomationRunId(task ecs.Task) string {
+	for _, containerOverride:= range task.Overrides.ContainerOverrides {
+		for _, environment:=range containerOverride.Environment {
+			if *environment.Name == "ZEBRUNNER_LAUNCH_UUID" {
+				return *environment.Value
+			}
+		}
+	}
+	return ""
+}
+
+func AbortTask(sess *sessionmap.Session, task *ecs.Task) {
+	conf := &config.Conf
+	requestUrl, err := url.ParseRequestURI(conf.ZebrunnerHost)
+	if err != nil {
+		log.WithError(err).Error("Failed to parse zebrunner base url")
+		return
+	}
+	requestUrl.Host = sess.Workspace + "." + requestUrl.Host
+
+	automationRunId := getAutomationRunId(*task)
+	if automationRunId ==""{
+		log.Error("Failed to obtain Automation Run Id")
+		return
+	}
+
+	requestUrl.Path = fmt.Sprintf("%s?id=%s", ABORT_API_PATH, automationRunId)
+		requestBody := map[string]interface{}{
+		"comment": "Launch finished",
+	}
+	log.Trace("request body to abort call: ", requestBody)
+
+	body, err := json.Marshal(requestBody)
+	if err != nil {
+		log.WithError(err).Error("Failed to marshal abort data")
+		return
+	}
+	req, err := http.NewRequest(http.MethodPost, requestUrl.String(), bytes.NewBuffer(body))
+	if err != nil {
+		log.WithError(err).Error("Failed to create request")
+	}
+	req.SetBasicAuth(conf.ZebrunnerIntegrationUser, conf.ZebrunnerIntegrationPassword)
+	req.Header.Add("Content-Type", "application/json")
+	log.Trace("req: ", req)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.WithError(err).Error("Failed to send request")
+		return
+	}
+
+	if resp.StatusCode != http.StatusNoContent {
+		data := map[string]interface{}{}
+		err = json.NewDecoder(resp.Body).Decode(&data)
+
+		if err != nil {
+			log.WithError(err).Error("Failed to track task abort")
+		}
+		log.WithFields(log.Fields{
+			"status":   resp.Status,
+			"response": data,
+		}).Error("Failed to track task abort!")
+		return
+	} else {
+		log.WithField("_taskId", sess.ID).WithField("workspace", sess.Workspace).WithField("request body", requestBody).Info("task aborted")
 	}
 }
