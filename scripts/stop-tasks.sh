7cd4ce2ba5dd95bf45958c0fba71cd85c525f704
dmtgrinevich <dgrinevich@solvd.com>
2023-05-22 07:01:40 +0000

    refactor(stop-tasks.sh): extended response message

--- scripts/stop-tasks.sh
+++ scripts/stop-tasks.sh
@@ -3,18 +3,22 @@
 
 # find out cluster name
 clusterLine=`grep "AWS_CLUSTER" ../router.env`
-cluster=${clusterLine: `expr match $clusterLine AWS_CLUSTER=`}
+cluster=${clusterLine: `expr match $clusterLine "AWS_CLUSTER="`}
 
-# get all tasks for current cluster and iterate by their AKR
-aws ecs list-tasks --cluster $cluster | jq -r '.[]' | while read taskAkr ; do
-    taskIdIndex=`expr match $taskAkr ^\"[a-z:0-9\-]*/$cluster/`
+# get all tasks for current cluster and iterate by their ARN
+aws ecs list-tasks --cluster $cluster | jq -r '.[]' | while read taskArn ; do
+    taskIdIndex=`expr match $taskArn ^\"[a-z:0-9\-]*/$cluster/`
     if [[ $taskIdIndex != 0 ]]
     then 
-        # parse taskId from full AKR
-        tmpTaskId=${taskAkr: taskIdIndex}
+        # parse taskId from full ARN
+        tmpTaskId=${taskArn: taskIdIndex}
         taskId=${tmpTaskId%%\"*}
+
         # stop task by taskId for certain cluster
-        response= aws ecs stop-task --cluster $cluster --task $taskId | jq '.task.taskArn, .task.lastStatus'
+        response= aws ecs stop-task --cluster $cluster --task $taskId | jq '.task.taskArn,
+                                                                             "Last status:", .task.lastStatus,
+                                                                             "Desired status:", .task.desiredStatus,
+                                                                             "Stopped reason:", .task.stoppedReason'
         echo "$response"
     fi
 done
