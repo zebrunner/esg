3708b0be34befb034b8685d58f6417b12f35af4b
Anatoliy Platonov <p4m.dev@gmail.com>
2023-05-29 12:52:42 +0000

    improved shell scripts to manage tasks via cli

--- scripts/stop-tasks.sh
+++ scripts/stop-tasks.sh
@@ -3,25 +3,21 @@
 
 # get base directory and cluster
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cluster=`$BASEDIR/cluster.sh`
 
 # get all tasks
-tasks=`$BASEDIR/list-tasks.sh`
+. $BASEDIR/list-tasks.sh
 
 # iterate tasks by their ARN
-echo $tasks | jq -r '.[]' | while read taskArn ; do
-    taskIdIndex=`expr match $taskArn ^\"[a-z:0-9\-]*/$cluster/`
-    if [[ $taskIdIndex != 0 ]]
-    then 
-        # parse taskId from full ARN
-        tmpTaskId=${taskArn: taskIdIndex}
-        taskId=${tmpTaskId%%\"*}
+echo $TASKS | jq -r '.[]' | while read taskArn ; do
+  # example of the taskArn:
+  # arn:aws:ecs:us-east-1:659932254483:task/esg-dev/50d8fcf7a7e24adeb4dca2fda5b600d7
+  taskId=`echo ${taskArn} | cut -d '/' -f 3 | cut -d '"' -f 1`
+  #echo taskId: $taskId
 
-        # stop task by taskId for certain cluster
-        response= aws ecs stop-task --cluster $cluster --task $taskId | jq '.task.taskArn,
-                                                                             "Last status:", .task.lastStatus,
-                                                                             "Desired status:", .task.desiredStatus,
-                                                                             "Stopped reason:", .task.stoppedReason'
-        echo "$response"
-    fi
+  if [ "$taskId" = "[" ] || [ "$taskId" = "]" ] || [ "$taskId" = "[]" ]; then
+    continue
+  fi
+
+  aws ecs stop-task --cluster $AWS_CLUSTER --task $taskId --reason "Stopped forcibly by admin" | jq '.task.taskArn, "Last status:", .task.lastStatus, "Desired status:", .task.desiredStatus, "Stopped reason:", .task.stoppedReason'
+  echo
 done
