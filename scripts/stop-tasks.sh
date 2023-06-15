71276c80f20edabfa8487d0b0da521a6c606b713
dmtgrinevich <dgrinevich@solvd.com>
2023-06-15 11:17:18 +0000

    refactor(stop-tasks.sh): deleted extra parse

--- scripts/stop-tasks.sh
+++ scripts/stop-tasks.sh
@@ -11,10 +11,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo $TASKS | jq -r '.[]' | while read taskArn ; do
   # example of the taskArn:
   # arn:aws:ecs:us-east-1:659932254483:task/esg-dev/50d8fcf7a7e24adeb4dca2fda5b600d7
-  # example of the taskId:
-  # 50d8fcf7a7e24adeb4dca2fda5b600d7
-  taskId=`echo ${taskArn} | cut -d '/' -f 3`
 
-  aws ecs stop-task --cluster $AWS_CLUSTER --task $taskId --reason "Stopped forcibly by admin" | jq '.[] | [{key:.taskArn, value: {containerInstanceArn, group, createdAt, stoppingAt, desiredStatus, lastStatus, stoppedReason, cpu, memory, containers: [.containers[] | {name,lastStatus}]}}] | from_entries'
+  aws ecs stop-task --cluster $AWS_CLUSTER --task $taskArn --reason "Stopped forcibly by admin" | jq '.[] | [{key:.taskArn, value: {containerInstanceArn, group, createdAt, stoppingAt, desiredStatus, lastStatus, stoppedReason, cpu, memory, containers: [.containers[] | {name,lastStatus}]}}] | from_entries'
   echo
 done
