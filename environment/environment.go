20334fd774c345c9ee6db07720bab0bf6093abfa
vdelendik <vdelendik@zebrunner.com>
2023-06-16 19:43:54 +0000

    #694: bump up to entrypoint:2.2-beta1

--- environment/environment.go
+++ environment/environment.go
@@ -26,7 +26,7 @@ const (
         cypressRecorderImage = imageRepo + "cypress-recorder:1.1"
 	appiumImage     = imageRepo + "appium:1.4.10"
 	cloneImage      = imageRepo + "git:latest"
-	entrypointImage = imageRepo + "entrypoint:2.1"
+	entrypointImage = imageRepo + "entrypoint:2.2-beta1"
 	mavenImage      = imageRepo + "m2-repo-carina:1.4"
 )
 
