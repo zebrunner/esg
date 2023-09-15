12756717bb6e052c78e6cb0c3d5a6992e3ae6198
dmtgrinevich <dgrinevich@solvd.com>
2023-09-15 14:43:01 +0000

    Refactored vnc handler, reduced log info

--- handlers/middlewares.go
+++ handlers/middlewares.go
@@ -136,7 +136,7 @@ func getSession(id string) (*sessionmap.Session, *utils.SeleniumError) {
 	}
 
 	if session.Status == sessionmap.SessionStopped {
-		return nil, utils.SessionStoppedErr(fmt.Errorf(string(session.StopReason)))
+		return session, utils.SessionStoppedErr(fmt.Errorf(string(session.StopReason)))
 	}
 
 	return session, nil
@@ -149,7 +149,7 @@ func getTask(id string) (*taskmap.Task, *utils.SeleniumError) {
 	}
 
 	if task.Status == taskmap.TaskStopped {
-		return nil, utils.TaskStoppedErr(fmt.Errorf(string(task.StopReason)))
+		return task, utils.TaskStoppedErr(fmt.Errorf(string(task.StopReason)))
 	}
 
 	return task, nil
