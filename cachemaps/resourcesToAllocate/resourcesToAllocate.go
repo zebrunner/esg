aa43425a7b44bb79b0969b8856b5f6d2184cd5d7
dmtgrinevich <dgrinevich@solvd.com>
2023-09-29 15:31:28 +0000

    refactor: Scale down calculation based on running, provisioning and preprovisioning tasks allocation

---
+++ cachemaps/resourcesToAllocate/resourcesToAllocate.go
@@ -0,0 +1,57 @@
+package resourcesToAllocate
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/zebrunner/esg/config"
+)
+
+type Resources struct {
+	UUID   string
+	Cpu    int64
+	Memory int64
+}
+
+func AddEntity(resources *Resources) error {
+	data, err := json.Marshal(&resources)
+	if err != nil {
+		return err
+	}
+
+	err = config.ResourcesConnection.Set(context.Background(), resources.UUID, data, 0).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func RemoveEntity(uuid string) error {
+	return config.ResourcesConnection.Del(context.Background(), uuid).Err()
+}
+
+func GetAllEntities() ([]*Resources, error) {
+	keys, err := config.ResourcesConnection.Keys(context.Background(), "*").Result()
+	if err != nil {
+		return nil, err
+	}
+
+	resources := make([]*Resources, len(keys))
+	for _, uuid := range keys {
+		data, err := config.ResourcesConnection.Get(context.Background(), uuid).Result()
+		if err != nil {
+			return nil, err
+		}
+
+		var resource Resources
+		err = json.Unmarshal([]byte(data), &resource)
+		if err != nil {
+			return nil, err
+		}
+
+		resources = append(resources, &resource)
+	}
+
+	return resources, nil
+}
--- service/taskRegistrar.go
+++ service/taskRegistrar.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ecs"
 	log "github.com/sirupsen/logrus"
+	"github.com/zebrunner/esg/cachemaps/resourcesToAllocate"
 	"github.com/zebrunner/esg/config"
 	"github.com/zebrunner/esg/environment"
 )
@@ -47,12 +48,15 @@ func registerTask(ctx context.Context, env environment.ExecutionEnvironment, wai
 	// TODO: convert existing hard-coded 25 retries into the queue or provisioning timeout: https://github.com/zebrunner/esg/issues/72
 	// [VD] "i" retry should be ~15 if instances can be started in 1 min and 25 if ~2 min
 	var outputErr error
-	for i := 0; i < 25; i++ {
-
+	markedToAllocate := false
+	for i := 0; true; i++ {
 		l := l.WithField("retry", i)
 
 		select {
 		case <-ctx.Done():
+			if markedToAllocate {
+				resourcesToAllocate.RemoveEntity(env.RouterUUID)
+			}
 			return
 		default:
 		}
@@ -76,7 +80,15 @@ func registerTask(ctx context.Context, env environment.ExecutionEnvironment, wai
 			}
 
 			if errStr == "ClientException: Tasks provisioning capacity limit exceeded." || strings.Contains(errStr, "ThrottlingException: Rate exceeded") {
-				sleepRateLimit := time.Duration(15 + rand.Intn(15))
+				if !markedToAllocate {
+					resources := env.CalculateResources()
+					err := resourcesToAllocate.AddEntity(resources)
+					if err != nil {
+						l.Info("Failed to cache resource allocation")
+					}
+					markedToAllocate = true
+				}
+				sleepRateLimit := time.Duration(20+rand.Intn(4)) * time.Second
 				time.Sleep(sleepRateLimit)
 			}
 
@@ -94,6 +106,10 @@ func registerTask(ctx context.Context, env environment.ExecutionEnvironment, wai
 			outputErr = fmt.Errorf("response doesn't contain tasks")
 			l.WithError(outputErr).Debug("Task register failed")
 			continue
+		}
+
+		if markedToAllocate {
+			resourcesToAllocate.RemoveEntity(env.RouterUUID)
 		}
 
 		// All is ok. We got task then we can return it.
