1e16a9da2c42a9f3648ee1e6cab3bb87affb7975
dmtgrinevich <dgrinevich@solvd.com>
2024-02-14 13:08:45 +0000

    Bumped up to mitmproxy:2.1

--- environment/environment.go
+++ environment/environment.go
@@ -29,7 +29,7 @@ const (
 	//public zebrunner ECR docker registry
 	imageRepo            = "public.ecr.aws/zebrunner/"
 	uploaderImage        = imageRepo + "uploader:3.5"
-	mitmImage            = imageRepo + "mitmproxy:2.0"
+	mitmImage            = imageRepo + "mitmproxy:2.1"
 	recorderImage        = imageRepo + "recorder:1.5"
 	cypressRecorderImage = imageRepo + "cypress-recorder:1.3"
 	appiumImage          = imageRepo + "appium:2.0.10"
