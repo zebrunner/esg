a571fcc5d6a867a2323e3815f78146196332b62d
dmtgrinevich <dgrinevich@solvd.com>
2024-03-26 12:23:00 +0000

    restricted override for e3s_dns env var in executor container

--- environment/genegic.go
+++ environment/genegic.go
@@ -124,8 +124,6 @@ func buildGeneric(workspace string, routerUUID string, caps *capabilities.Capabi
 		Essential:  true,
 		Env: map[string]string{
 			"COMMAND": launchCommand.ToPrimitive(),
-			"UUID":    routerUUID,
-			"E3S_DNS": config.Conf.AwsEsgDns,
 		},
 		Mounts:           mounts,
 		WorkingDirectory: workDir,
@@ -149,6 +147,8 @@ func buildGeneric(workspace string, routerUUID string, caps *capabilities.Capabi
 		}
 	}
 
+	executorContainer.Env["UUID"] = routerUUID
+	executorContainer.Env["E3S_DNS"] = config.Conf.AwsEsgDns
 	executorContainer.SetCpu(&caps.Cpu, 1024, conf.MaxCpu)
 	executorContainer.SetMemory(&caps.Memory, 1024, conf.MaxMemory)
 
