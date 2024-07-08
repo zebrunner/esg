8ef732aa3cab62fc0a907aa2b880b49f4a2309de
dmtgrinevich <dgrinevich@solvd.com>
2024-07-08 12:23:38 +0000

    fixed misprint

--- config/config.go
+++ config/config.go
@@ -112,7 +112,7 @@ func init() {
 	flag.StringVar(&Conf.S3AwsSecretAccessKey, "s3-aws-secret-access-key", "", "Secret key for S3 bucket")
 
 	flag.StringVar(&Conf.LogLevel, "log-level", "debug", "Desired log level. Valid levels: `panic`, `fatal`, `error`, `warning`, `info`, `debug`, `trace`")
-	flag.StringVar(&Conf.AwsLogsGroup, "aws-logs-grop", "", "Aws cloud watch logs group")
+	flag.StringVar(&Conf.AwsLogsGroup, "aws-logs-group", "", "Aws cloud watch logs group")
 
 	flag.Float64Var(&Conf.ReserveInstancesPercent, "reserve-instances-percent", 0.25, "Reserved cluster capacity quota during scale up and down operations")
 	flag.Int64Var(&Conf.ReserveMaxCapacity, "reserve-max-capacity", 5, "Reservation instance limit")
